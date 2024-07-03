6e214996031d7d8cd703f4044797c5aeb22ed905
Pgama31 <pgama31@gmail.com>
2024-07-03 16:31:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = '255597916992-ghod19eihjd7s980tot66656ssqqd9q1.apps.googleusercontent.com.';  // Substitua pelo seu Client ID
+const CLIENT_ID = '255597916992-ghod19eihjd7s980tot66656ssqqd9q1.apps.googleusercontent.com';  // Substitua pelo seu Client ID
 const REDIRECT_URI = 'https://pgama31.github.io/oauth2callback';
 const SCOPE = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets';
 
