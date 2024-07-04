94acb838930c5aa08abce6c3d722f81df0b8dfbd
Pgama31 <pgama31@gmail.com>
2024-07-04 15:01:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = '255597916992-ghod19eihjd7s980tot66656ssqqd9q1.apps.googleusercontent.com';
 const REDIRECT_URI = 'https://pgama31.github.io/oauth2callback';
-const SCOPE = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/spreadsheets';
+const SCOPE = 'https://www.googleapis.com/auth/calendar';
 
 // Função para criar um evento no Google Calendar
 function createCalendarEvent(authToken, eventDetails) {
