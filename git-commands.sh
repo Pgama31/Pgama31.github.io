624ce3bccb329d5f38c299699f21ad861341269b
Seu Nome no GitHub <seu-email-no-github@example.com>
2024-07-12 13:27:34 +0000

    Resolvendo conflitos de mesclagem

---
+++ git-commands.sh
@@ -0,0 +1,13 @@
+mkdir hidraulica1
+
+cd hidraulica1
+
+git init
+git add index.html
+git commit -m "Adicionar arquivo inicial"
+git remote add origin https://github.com/pgama31/https://github.com/Pgama31/Pgama31.github.io
+git branch -M main
+git push -u origin main
+
+
+
